5c4baa68bd78c96d6cccc88d632c7eda6f993f6a
Replit user <>
2023-04-20 06:56:32 +0000

    redux-saga setup

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,20 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 
 export type AppState = ReturnType<typeof rootReducer>
 
+function* rootSaga(){
+}
+
+const sagaMiddleware=createSagaMiddleware()
+
 const store = createStore(
   rootReducer,
-  composeWithDevTools()
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
+
+sagaMiddleware.run(rootSaga)
 
 export default store;
